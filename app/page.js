6fcf8c91f6bba042c5a3aa42aaa7482b4016d5e0
Sebabin <sebastiancrespin279@gmail.com>
2023-08-31 17:44:05 -0600

    started adding the quiz scoring functionality

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Quiz from '@/components/Quiz'
 export default function Home() {
   return (
--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -1,6 +1,52 @@
+
+import { useState } from "react";
 import { questions } from "@/utils/questionData";
+/**each question is worth a max of 10 points */
 export default function Quiz () {
-    console.log(questions)
+    const [typeA,setTypeA] = useState(0);
+    const [typeB,setTypeB] = useState(0);
+    const [balanced,setBalanced] = useState(0);
+    let potentialPoints;
+    let potentialPointsCounter = 0;
+    questions.forEach(question => {
+       potentialPointsCounter = potentialPointsCounter + 15;
+    });
+    potentialPoints = potentialPointsCounter;
+ console.log(potentialPoints);
+ const handleQuestionAnswer = (event,question) => {
+    event.preventDefault();
+    console.log(event.target)
+    const isUserCorrect = event.target.getAttribute('data-correct');
+    const answerType = event.target.getAttribute('data-answertype');
+    console.log(isUserCorrect);
+    console.log(answerType);
+    const correctType = question.correctType;
+    if(isUserCorrect && answerType === correctType) {
+        setBalanced(balanced + 15);
+        return;
+    }else if(!isUserCorrect && correctType === 'far right') {
+          switch(answerType) {
+            case'start left':
+            setTypeB(typeB + 5);
+            setTypeA(typeA - 5);
+            setBalanced(balanced - 5);
+            break;
+            case'mid left':
+            setTypeB(typeB + 10);
+            setTypeA(typeA - 10);
+            setBalanced(balanced - 10);
+            break;
+            case'far left':
+            setTypeB(typeB + 15);
+            setTypeA(typeA - 15);
+            setBalanced(balanced - 15);
+            break;
+          }
+    }
+
+    
+
+ };
     return(
     <form>
 
@@ -17,12 +63,16 @@ export default function Quiz () {
                 
                 <button
                
+                key={Math.random()}
                  className="mt-2 mb-2"
-                 data-correct={answer.correct}
-                 data-answertype={answer.answerType}
+                 
                  >
                     <span>{answer.answerType}</span>
-                    <p className="text-center text-xs">
+                    <p 
+                    onClick={(event) => handleQuestionAnswer(event,question)}
+                    data-correct={answer.correct}
+                    data-answertype={answer.answerType}
+                    className="text-center text-xs">
                     {answer.answerTxt}
                     </p>
                 </button>
@@ -40,12 +90,17 @@ export default function Quiz () {
             {question.ques2.answers ? question.ques2.answers.map(answer => (
                 
                 <button
+                
+                key={Math.random()}
                 className="mt-2 mb-2" 
-                data-correct={answer.correct}
-                data-answertype={answer.answerType}
+                
                 >
                     <span>{answer.answerType}</span>
-                    <p className="text-center text-xs">
+                    <p 
+                    onClick={(event) => handleQuestionAnswer(event,question)}
+                    data-correct={answer.correct}
+                    data-answertype={answer.answerType}
+                    className="text-center text-xs">
                         {answer.answerTxt}
                     </p>
                 </button>
