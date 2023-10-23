42ba8ee10e8ff3eb5c0d8f0423f207bcadf4cc9b
Sebabin <sebastiancrespin279@gmail.com>
2023-10-22 22:39:59 -0600

    started styling products page

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -64,16 +64,25 @@ const [stripeData,setStripeData] = useState(null);
       
 
   return (
-    <div className="bg-white shadow-md rounded-lg overflow-hidden">
+    <div className="bg-[#999595] shadow-md rounded-lg overflow-hidden">
       <div className="p-4">
-        <h2 className="text-xl font-semibold mb-2">{product.productTitle}</h2>
-        <p className="text-gray-500 mb-2">Quiz Title: {product.quiz.quizTitle}</p>
-        <p className="text-gray-500 mb-4">{product.marketingCopy}</p>
+        <h2 className="text-xl text-white  font-semibold mb-2">{product.productTitle}</h2>
+        <p className="text-[#fde1e2]  mb-2">Quiz Title: {product.quiz.quizTitle}</p>
+        <p className="text-[#fde1e2]  mb-4">{product.marketingCopy}</p>
         <div className="flex justify-between">
-          <span className="text-gray-700 font-semibold">Price: ${product.price}</span>
+          <span className="text-white font-semibold">Price: ${product.price}</span>
           <button
            onClick={handleBuyNowClick}
-            className="px-4 py-2 bg-indigo-500 text-white rounded-full hover:bg-indigo-600 transition duration-300 ease-in-out"
+            className="px-4 
+            py-2 
+            rounded-full  
+            transition duration-300 
+            ease-in-out
+            bg-[#849b9f]
+            hover:bg-[#fde1e2]
+            hover:text-white
+            text-white
+            "
           >
             Buy Now
           </button>
