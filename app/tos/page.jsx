96f2e80a377c37797abd598e68cb0925f75affef
Sebabin <sebastiancrespin279@gmail.com>
2023-10-23 22:03:20 -0600

    added footer structure

---
+++ app/tos/page.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+
+const Tos = () => {
+  return (
+    <section className="p-2 w-[100%] flex flex-col justify-center">
+      <h1 className="text-3xl font-bold text-center mb-8">
+        Terms of Service for Villainy Quiz Subscription
+      </h1>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Acceptance of Terms</h2>
+        <p>
+          By using the Villainy Quiz Subscription service "Service," you agree to abide by these
+          terms and conditions.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Description of Service</h2>
+        <p>
+          The Villainy Quiz Subscription offers subscribers access to a collection of quizzes
+          designed to explore individuals' tendencies toward various types of villainy. Subscribers
+          can administer tests and receive results via email.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Subscription and Billing</h2>
+        <ul className="list-disc list-inside">
+          <li>Subscriptions are available on a monthly or annual basis, with clear pricing outlined on our website.</li>
+          <li>Payment can be made via credit card or other accepted methods.</li>
+          <li>Subscriptions are automatically renewed, and users can cancel anytime.</li>
+        </ul>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">User Responsibilities</h2>
+        <p>
+          Users are expected to use the Service in compliance with all applicable laws and must
+          respect the privacy and rights of other users.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Privacy and Data Usage</h2>
+        <p>
+          We collect, store, and protect user data as outlined in our Privacy Policy. Users' data
+          privacy is a top priority.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Intellectual Property</h2>
+        <p>
+          All content, quizzes, and related intellectual property on the Service are owned by us.
+          Users may not reproduce or distribute such content without permission.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">User Conduct</h2>
+        <p>
+          Users are prohibited from engaging in any fraudulent, harmful, or malicious behavior within
+          the Service. Violations may result in suspension or termination.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Termination</h2>
+        <p>
+          Users can cancel their subscriptions at any time. We reserve the right to terminate user
+          access in cases of abuse, non-payment, or violations of these terms.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Refund and Cancellation Policy</h2>
+        <p>
+          Refund requests are considered on a case-by-case basis. Users can cancel subscriptions via
+          their account settings.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Disclaimers</h2>
+        <p>
+          The results of quizzes provided by this Service are intended for entertainment and
+          informational purposes only. They do not provide professional psychological or medical advice.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Limitation of Liability</h2>
+        <p>
+          Our company is not liable for any direct, indirect, or consequential damages arising from the
+          use of our Service. We do not guarantee the accuracy of quiz results.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Changes to Terms</h2>
+        <p>
+          We reserve the right to modify these terms and will notify users of changes through the
+          Service or via email.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Contact Information</h2>
+        <p>
+          For questions, concerns, or legal notices, you can contact us at contact@villainyquizsubscription.com.
+        </p>
+      </div>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold">Entire Agreement</h2>
+        <p>
+          These terms represent the entire agreement between users and the Villainy Quiz Subscription service.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Tos;
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,37 @@
+import Link from "next/link";
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     
     return (
-      <footer className="bg-[#849b9f] p-4 text-center">
-        <div className="container mx-auto">
-          <p className="text-[#fde1e2]">
+      <footer className="bg-[#849b9f] p-2 text-center w-[100%]">
+        <div className="container mx-auto w-[100%]">
+          <div className="flex flex-row justify-evenly mb-2">
+            <Link
+            href="/tos" 
+            className="block px-4 py-2 text-[.7em] text-white hover:bg-gray-100 min-[540px]:text-[1.2em] underline" 
+            >Terms of Service</Link>
+            <Link
+            href="/products" 
+            className="block px-4 py-2 text-[.7em] text-white hover:bg-gray-100 min-[540px]:text-[1.2em] underline" 
+            >Products</Link>
+            <a
+             className="block px-4 py-2 text-[.7em] text-white hover:bg-gray-100 min-[540px]:text-[1.2em] underline" 
+             target="_blank"
+              href="https://advocate-for-rights-and-knowledge-of-americans-arka.ghost.io">
+                Blog
+              </a>
+          </div>
+          <div className="flex flex-col justify-between items-center w-full min-[540px]:flex-row">
+          <p className="text-[#fde1e2] text-[.6em] ">
             &copy; {currentYear} VillainyTest
           </p>
+          <p className="text-[#fde1e2] text-[.7em]">Built with ✊ by&nbsp;<a
+          className="text-[#fee084]"
+           href="https://topdev.tech/" 
+           target="_blank">TopDev_Tech
+           </a>
+          </p>
+          </div>
         </div>
       </footer>
     );
